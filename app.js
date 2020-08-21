8b379e59d0a76778551d9edab8161a5cba865e58
Srushti <srushti.raybhoge1999@gmail.com>
2020-08-21 22:06:47 +0530

    final commit

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 (function () {
   "use strict";
 
-  angular.module("MsgShow", []).controller("MsgShowController", msgController);
+  angular
+    .module("MessageApp", [])
+    .controller("MessageAppController", msgController);
   msgController.$inject = ["$scope"];
   function msgController($scope) {
-    $scope.name = "Srush";
-    $scope.state = "answer";
-    $scope.sayMessage = function () {
-      return "Srush likes pav bhaji";
-    };
-    $scope.feedme = function () {
-      $scope.state = "apply";
+    $scope.message = "";
+    $scope.message1 = "";
+    $scope.comment = "";
+    $scope.items = "";
+    $scope.showMessage = function () {
+      $scope.message = "";
+      $scope.message1 = "";
+      $scope.comment = "";
+      var item = $scope.items.split(",");
+      var count = item.length;
+      for (let index = 0; index < item.length; index++) {
+        const element = item[index];
+        if (element == " ") {
+          $scope.comment = ", , will not be considered as an item.";
+          count--;
+        }
+      }
+      if (count <= 3) {
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.message1 = "Too much!";
+      }
     };
   }
 })();
